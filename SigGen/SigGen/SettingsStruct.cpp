ef4cbcdb590a78d75d5b32303facea9263ca3689
Besemer, Jim <Jim.Besemer@coherent.com>
2018-07-27 13:43:56 -0700

    clear SigGen

--- SigGen/SigGen/SettingsStruct.cpp
+++
@@ -1,9 +0,0 @@
-#include "SettingsStruct.h"
-#include "PersistentSettings.h"
-#include "CommandProcessing.h"
-#include "AnalogWriter.h"
-
-void SettingsStructV2::SetDefaults(){
-	VariableGainEnabled = CommandProcessing::VariableGainEnabledDefault;
-	Gain = UNITY_GAIN;
-}
