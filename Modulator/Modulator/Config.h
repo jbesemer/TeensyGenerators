fcb05a54f121f9f1635fee7413bc8a67947e4c85
jbesemer <james.besemer@gmail.com>
2018-08-05 11:23:25 -0700

    TeensyModulator.h to Modulator.h

--- Modulator/Modulator/Config.h
+++ Modulator/Modulator/Config.h
@@ -3,7 +3,7 @@
 // Config.h -- global configuration switch and options
 
 #include "TeensyCommon.h"
-#include "TeensyModulator.h"
+#include "Modulator.h"
 
 
 // individual features/commands 
@@ -17,6 +17,4 @@
 #define ASSEMBLYNAME "Modulator"
 #define VERSION "1.0.1"
 #define MODEL "Teensy 3.1"
-#define DATE "5-Aug-2018 11:15"
-
-
+#define DATE "5-Aug-2018 11:26"
