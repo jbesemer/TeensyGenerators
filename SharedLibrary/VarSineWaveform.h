a807c7cae7121914fff7d840919527efb4fa6e55
jbesemer <james.besemer@gmail.com>
2018-08-05 10:13:25 -0700

    cosmetic

--- SharedLibrary/VarSineWaveform.h
+++ SharedLibrary/VarSineWaveform.h
@@ -9,9 +9,9 @@
 // a SineWaveform which is associated with several Pots, each of which
 // controls one of the three primary waveform parameters:
 //
-//		P1 -- amplitude
-//		P2 -- offset
-//		P3 -- frequency
+//		ampPin -- amplitude
+//		offPin -- offset
+//		freqPin -- frequency
 // 
 // when the waveform is defined, the number of samples is fixed, as are the
 // ranges of each of the 3 parameters. The settings vary 
